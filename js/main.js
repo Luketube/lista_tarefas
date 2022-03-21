3cce93d6f8a9dde01f1ed52facd49a690056b439
Lucas Farias <lucasfarias48@hotmail.com>
2022-03-21 16:56:38 -0300

    Atribuido função de remover tarefa

--- js/main.js
+++ js/main.js
@@ -1,23 +1,34 @@
 const botaoAdicionar = document.querySelector('.adiciona_tarefa_button');
 const inputTarefa = document.querySelector('.adiciona_tarefa_input');
-const listaTarefas = document.querySelector('.lista_tarefas');
-
+const listaTarefas = document.querySelector('.lista_tarefas_conteudo');
 
 function conteudoTarefa (nomeTarefa){
-
-    var conteudo = `<hr class="linha">
+    var conteudo = `<li class="li_tarefa">
+    <hr class="linha">
     <div class="tarefa">
         <div class="tarefa_conteudo">
             <input class="tarefa_conteudo_checkbox" type="checkbox" id="tarefaFeita" name="tarefaFeita">
             <label class="tarefa_conteudo_nome" for="tarefaFeita">${nomeTarefa}</label>
         </div>
-        <img class="icon_remover" src="./img/trash.svg" alt="Icone de lixeira">
+        <button class="botao_remover"><img src="./img/trash.svg" alt="Icone de lixeira"></button>
     </div>
-    <hr class="linha">`
+    <hr class="linha">
+    </li>`
 
     listaTarefas.insertAdjacentHTML('beforeend', conteudo);
 
+    var tarefas = document.querySelectorAll('.botao_remover');
 
+    var botaoRemover = tarefas[tarefas.length - 1]
+
+    botaoRemover.addEventListener('click', () => {
+        removeTarefa(botaoRemover);
+    })
+
+}
+
+function removeTarefa(botao){
+    botao.parentNode.parentNode.remove();
 }
 
 function adicionaTarefa(){
@@ -27,4 +38,3 @@ function adicionaTarefa(){
 
 
 botaoAdicionar.addEventListener('click', adicionaTarefa);
-
